a44d22363ac6bb470eb7c9babfc3cf359eebf9cf
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2023-10-20 12:31:59 +0200

    Initial commit

---
+++ src/test/java/pl/akademiaqa/pages/HomePage.java
@@ -0,0 +1,18 @@
+package pl.akademiaqa.pages;
+
+import com.microsoft.playwright.Page;
+import lombok.Getter;
+import pl.akademiaqa.pages.sections.TopMenuAndSearchSection;
+
+public class HomePage {
+
+    private Page page;
+    @Getter
+    private TopMenuAndSearchSection topMenuAndSearchSection;
+
+    public HomePage(Page page) {
+        this.page = page;
+        this.topMenuAndSearchSection = new TopMenuAndSearchSection(page);
+    }
+
+}
